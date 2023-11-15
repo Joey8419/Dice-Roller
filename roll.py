4cd488bf2032b4da476cd540762ebf0fbcbca674
Joey8419 <jnelson297@gmail.com>
2023-11-15 14:00:14 -0500

    added changes

--- roll.py
+++ roll.py
@@ -48,24 +48,48 @@ dice = []
 total = 0
 # Ask user for input for a number of dice
 # type cast input as an integer
-number_of_dice = int(input("How many dice?: "))
 
-# Generate a random number between (1, 6)
-# random.randint(1, 6)
-# Then append number to list of dice
-# Do this number amount of times depending on how many dice the user enters in
-# Now add code to for loop
-for die in range(number_of_dice):
-    dice.append(random.randint(1, 6))
+while True:
+    number_of_dice = int(input("How many dice?: "))
+    # Generate a random number between (1, 6)
+    # random.randint(1, 6)
+    # Then append number to list of dice
+    # Do this number amount of times depending on how many dice the user enters in
+    # Now add code to for loop
+    for die in range(number_of_dice):
+        dice.append(random.randint(1, 6))
 
 
-# Create nested for loop 
+    # Create nested for loop with the outer for loop will be in charge of the dice
+    for die in range(number_of_dice):
+        # Inner for loop will be in charge of printing every tuple
+        # Get value in dictionary (name of dictionary) at a given key at index of die
+        # Depending on what  user input is, die will begin counting at 1 and then increment
+        for line in dice_art.get(dice[die]):
+            # Print the line
+            print(line)   
 
-# Calculate total and iterate and sum all of the elements within the list
-# Use another for loop
-for die in dice:
-    total += die # die = current value
-print (f"total: {total}")
+    # Calculate total and iterate and sum all of the elements within the list
+    # Use another for loop
+    for die in dice:
+        total += die # die = current value
+    print (f"total: {total}")
+
+    while True:
+        #Ask user if they want to roll dice again
+        roll_again = input("Do you want to roll the dice again? (yes/no)").lower()
+
+        # Check if the user wants to roll again
+        if roll_again == 'yes':
+            print("Roll again!")
+            break
+            # If the user enters something other than yes, exit the loop
+            
+        elif roll_again == "no":
+            quit("See ya wouldnt wanna be ya!")
+        else:
+            print("wrong choice. Enter yes or no")
+
 
 # roll a "die" some number of times.
 # roll - run it once and go into a loop
